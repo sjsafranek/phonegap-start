828d7d06e522ba847664302f8909d799a4a932d4
stefan safranek <stefan@stefan.stefan>
2018-04-21 08:41:11 -0700

    geolocation plugin

--- www/js/index.js
+++ www/js/index.js
@@ -16,6 +16,28 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+
+
+ var onSuccess = function(position) {
+        alert('Latitude: '          + position.coords.latitude          + '\n' +
+              'Longitude: '         + position.coords.longitude         + '\n' +
+              'Altitude: '          + position.coords.altitude          + '\n' +
+              'Accuracy: '          + position.coords.accuracy          + '\n' +
+              'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+              'Heading: '           + position.coords.heading           + '\n' +
+              'Speed: '             + position.coords.speed             + '\n' +
+              'Timestamp: '         + position.timestamp                + '\n');
+    };
+
+ // onError Callback receives a PositionError object
+ //
+ function onError(error) {
+    alert('code: '    + error.code    + '\n' +
+          'message: ' + error.message + '\n');
+ }
+
+
+
 var app = {
     // Application Constructor
     initialize: function() {
@@ -34,6 +56,8 @@ var app = {
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
+
+           navigator.geolocation.getCurrentPosition(onSuccess, onError);
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
